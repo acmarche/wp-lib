19c86ecb66dad48e41c479c215908b13fdd7e3cb
jfsenechal <jfsenechal@gmail.com>
2020-12-08 09:34:43 +0100

    comment

--- Pivot/Repository/HadesRepository.php
+++ Pivot/Repository/HadesRepository.php
@@ -23,6 +23,11 @@ class HadesRepository
         $this->cache = Cache::instance();
     }
 
+    /**
+     * https://www.ftlb.be/rss/xmlinterreg.php?pays=9&offre=evenements&quoi=tout
+     * @return string
+     * @throws \Psr\Cache\InvalidArgumentException
+     */
     public function getEvents(): string
     {
         return $this->cache->get(
