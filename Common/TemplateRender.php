64a6d8a04e868b81a45e79e2dd63d744fc947a17
jfsenechal <jfsenechal@gmail.com>
2020-12-09 16:55:13 +0100

    extract logic pour react

---
+++ Common/TemplateRender.php
@@ -0,0 +1,69 @@
+<?php
+
+
+namespace AcMarche\Common;
+
+
+use AcMarche\Bottin\Bottin;
+use AcMarche\Bottin\Repository\BottinRepository;
+use AcMarche\Theme\Inc\Router;
+
+class TemplateRender
+{
+    public static function renderCategory(\WP_Query $wp_query): string
+    {
+        $twig             = Twig::LoadTwig();
+        $bottinRepository = new BottinRepository();
+
+        $cat_ID      = get_queried_object_id();
+        $description = category_description($cat_ID);
+        $title       = single_cat_title('', false);
+
+        $posts = $wp_query->get_posts();
+
+        $args     = ['parent' => $cat_ID, 'hide_empty' => false];
+        $children = get_categories($args);
+        array_map(
+            function ($category) {
+                $category->permalink = get_category_link($category->term_id);
+                $category->id        = $category->term_id;
+            },
+            $children
+        );
+
+        $fiches           = [];
+        $categoryBottinId = get_term_meta($cat_ID, \BottinCategoryMetaBox::KEY_NAME, true);
+        if ($categoryBottinId) {
+            $fiches = $bottinRepository->getFichesByCategory($categoryBottinId);
+        }
+
+        $all = array_merge($posts, $fiches);
+
+        array_map(
+            function ($post) {
+                if ($post instanceof \WP_Post) {
+                    $post->excerpt   = $post->post_excerpt;
+                    $post->permalink = get_permalink($post->ID);
+                } else {
+                    $post->fiche      = true;
+                    $post->excerpt    = Bottin::getExcerpt($post);
+                    $post->permalink  = Router::getUrlFicheBottin($post);
+                    $post->post_title = $post->societe;
+                }
+            },
+            $all
+        );
+
+        $content = $twig->render(
+            'category/category.html.twig',
+            [
+                'title'       => $title,
+                'description' => $description,
+                'children'    => $children,
+                'posts'       => $all,
+            ]
+        );
+
+        return $content;
+    }
+}
