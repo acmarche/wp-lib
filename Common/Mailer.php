b44736cb4a001ee2967616709def1de211f416f5
jfsenechal <jfsenechal@gmail.com>
2020-12-09 13:35:11 +0100

    util mailer

---
+++ Common/Mailer.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace AcMarche\Common;
+
+
+class Mailer
+{
+    public static function sendError(string $subject, string $message)
+    {
+        Env::loadEnv();
+        $to = $_ENV['WEBMASTER_EMAIL'];
+        wp_mail($to, $subject, $message);
+    }
+}
