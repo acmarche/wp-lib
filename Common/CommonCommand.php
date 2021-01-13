afdd5c610397aebb50da19c193263490e1ae1f7f
jfsenechal <jfsenechal@gmail.com>
2021-01-13 12:18:14 +0100

    add command

---
+++ Common/CommonCommand.php
@@ -0,0 +1,46 @@
+<?php
+
+
+namespace AcMarche\Common;
+
+use AcMarche\Theme\Inc\Router;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class CommonCommand extends Command
+{
+    protected static $defaultName = 'marche:do';
+
+    /**
+     * @var SymfonyStyle
+     */
+    private $io;
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Effectuer une recherche')
+            ->addArgument('query', InputArgument::REQUIRED, 'mot clef pour la description');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $this->io = new SymfonyStyle($input, $output);
+
+        $query = $input->getArgument('query');
+        if ( ! $query) {
+            $this->io->error('Entrez un mot clef');
+
+            return Command::FAILURE;
+        }
+
+        $router = new Router();
+        $router->flushRoutes();
+
+        return Command::SUCCESS;
+    }
+
+}
