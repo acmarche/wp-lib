e052a394e43eccffad5b1ca136777f936fbd1648
jfsenechal <jfsenechal@gmail.com>
2021-01-26 16:49:45 +0100

    page alert

--- Common/WpRepository.php
+++ Common/WpRepository.php
@@ -2,11 +2,23 @@
 
 namespace AcMarche\Common;
 
+use AcMarche\Theme\Inc\Theme;
 use WP_Post;
 use WP_Query;
 
 class WpRepository
 {
+    public static function getPageAlert(): ?WP_Post
+    {
+        $query = new WP_Query(array("page_id" => Theme::PAGE_ALERT, "post_status" => 'publish', 'post_type' => 'page'));
+        $post  = $query->get_posts();
+        if (count($post) > 0) {
+            return $post[0];
+        }
+
+        return null;
+    }
+
     /**
      * @param int $max
      *
@@ -20,46 +32,46 @@ class WpRepository
         foreach ($sites as $siteId => $name) :
             switch_to_blog($siteId);
 
-        $args = array(
+            $args = array(
                 'category_name' => 'quoi-de-neuf-principal,focus-principal',
                 'orderby'       => 'title',
                 'order'         => 'ASC',
             );
 
-        if ($siteId == 1) {
-            $args = array(
+            if ($siteId == 1) {
+                $args = array(
                     'category_name' => 'quoi-de-neuf',
                     'orderby'       => 'title',
                     'order'         => 'ASC',
                 );
-        }
+            }
 
-        $querynews = new WP_Query($args);
+            $querynews = new WP_Query($args);
 
-        while ($querynews->have_posts()) :
+            while ($querynews->have_posts()) :
 
                 $post = $querynews->next_post();
-        $id   = $post->ID;
+                $id   = $post->ID;
 
-        if (has_post_thumbnail($id)) {
-            $attachment_id      = get_post_thumbnail_id($id);
-            $images             = wp_get_attachment_image_src($attachment_id, 'original');
-            $post_thumbnail_url = $images[0];
-        } else {
-            $post_thumbnail_url = get_template_directory_uri().'/assets/images/404.jpg';
-        }
+                if (has_post_thumbnail($id)) {
+                    $attachment_id      = get_post_thumbnail_id($id);
+                    $images             = wp_get_attachment_image_src($attachment_id, 'original');
+                    $post_thumbnail_url = $images[0];
+                } else {
+                    $post_thumbnail_url = get_template_directory_uri().'/assets/images/404.jpg';
+                }
 
-        $post->post_thumbnail_url = $post_thumbnail_url;
+                $post->post_thumbnail_url = $post_thumbnail_url;
 
-        $permalink       = get_permalink($id);
-        $post->permalink = $permalink;
+                $permalink       = get_permalink($id);
+                $post->permalink = $permalink;
 
-        $post->blog_id = $siteId;
-        $post->blog    = $name;
-        $post->color   = MarcheConst::COLORS[$siteId];
+                $post->blog_id = $siteId;
+                $post->blog    = $name;
+                $post->color   = MarcheConst::COLORS[$siteId];
 
-        $news[] = $post;
-        endwhile;
+                $news[] = $post;
+            endwhile;
 
         endforeach;
         wp_reset_postdata();
@@ -73,6 +85,7 @@ class WpRepository
             }
         }
         $news = array_values($news);
+
         return $news;
     }
 }
