30360cc0a1c0ae02093c40aaaa6759fcda6d3af6
jfsenechal <jfsenechal@gmail.com>
2021-02-26 16:57:23 +0100

    if no acpu

--- Common/Cache.php
+++ Common/Cache.php
@@ -4,6 +4,7 @@
 namespace AcMarche\Common;
 
 use Symfony\Component\Cache\Adapter\ApcuAdapter;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 use Symfony\Contracts\Cache\CacheInterface;
 
 class Cache
@@ -13,19 +14,35 @@ class Cache
 
     public static function instance(): CacheInterface
     {
-        return new ApcuAdapter(
+        if (extension_loaded('apc') && ini_get('apc.enabled')) {
+            return new ApcuAdapter(
+            // a string prefixed to the keys of the items stored in this cache
+                $namespace = 'newmarche',
 
-        // a string prefixed to the keys of the items stored in this cache
-            $namespace = 'newmarche',
+                // the default lifetime (in seconds) for cache items that do not define their
+                // own lifetime, with a value 0 causing items to be stored indefinitely (i.e.
+                // until the APCu memory is cleared)
+                $defaultLifetime = 3600,
 
-            // the default lifetime (in seconds) for cache items that do not define their
-            // own lifetime, with a value 0 causing items to be stored indefinitely (i.e.
-            // until the APCu memory is cleared)
-            $defaultLifetime = 3600,
+                // when set, all keys prefixed by $namespace can be invalidated by changing
+                // this $version string
+                $version = null
+            );
+        } else {
+            return new FilesystemAdapter(
+            // a string used as the subdirectory of the root cache directory, where cache
+            // items will be stored
+                $namespace = 'newmarche2',
 
-            // when set, all keys prefixed by $namespace can be invalidated by changing
-            // this $version string
-            $version = null
-        );
+                // the default lifetime (in seconds) for cache items that do not define their
+                // own lifetime, with a value 0 causing items to be stored indefinitely (i.e.
+                // until the files are deleted)
+                $defaultLifetime = 3600,
+
+                // the main cache directory (the application needs read-write permissions on it)
+                // if none is specified, a directory is created inside the system temporary directory
+                $directory = null
+            );
+        }
     }
 }
