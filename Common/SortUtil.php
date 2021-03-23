8a98fa5c9ecf579295d9ccb3a3432944f835057f
jfsenechal <jfsenechal@gmail.com>
2021-03-23 16:23:15 +0100

    sort news

--- Common/SortUtil.php
+++ Common/SortUtil.php
@@ -4,6 +4,8 @@
 namespace AcMarche\Common;
 
 
+use WP_Post;
+
 class SortUtil
 {
     public static function sortPosts(array $posts): array
@@ -19,6 +21,29 @@ class SortUtil
                     }
 
                     return ($titleA < $titleB) ? -1 : 1;
+                }
+            }
+        );
+
+        return $posts;
+    }
+
+    /**
+     * @param WP_Post[] $posts
+     *
+     * @return WP_Post[]
+     */
+    public static function sortByPosition(array $posts):array
+    {
+        usort(
+            $posts,
+            function ($postA, $postB) {
+                {
+                    if ($postA->order == $postB->order) {
+                        return 0;
+                    }
+
+                    return ($postA->order < $postB->order) ? -1 : 1;
                 }
             }
         );
