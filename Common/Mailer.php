26a61c9d1a8e7ed9fb9cd082a4233a33bbadc4af
jfsenechal <jfsenechal@gmail.com>
2021-06-30 11:20:28 +0200

    adl inscription

--- Common/Mailer.php
+++ Common/Mailer.php
@@ -11,4 +11,14 @@ class Mailer
         $to = $_ENV['WEBMASTER_EMAIL'];
         wp_mail($to, $subject, $message);
     }
+
+    public static function sendInscription(string $nom, string $prenom, string $email, bool $rgpd)
+    {
+        Env::loadEnv();
+        $to = $_ENV['ADL_EMAIL'];
+        $subject = 'Inscription newsletter';
+        $message = $nom.' '.$prenom.' souhaite s\'inscrire avec le mail: '.$email;
+
+        wp_mail($to, $subject, $message);
+    }
 }
